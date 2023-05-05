e536c261764cbc12669067225072381fc283a56b
AmandaFrisk <amanda.frisk33@gmail.com>
2023-05-05 11:43:45 -0600

    comment in Home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,14 +3,15 @@ import React from 'react'
 // import Navbar1 from '../components/Navbar1'
 
 // import Hero from '../components/Hero'
-import Testimonial from '../components/Testimonial'
+// import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
     <>
+    {/*  Commented these out so we can see our work w/ everyones unfinished components AF */}
     {/* <Navbar1/> */}
     {/* <Hero/> */}
-     <Testimonial/>
+     {/* <Testimonial/> */}
   </>
   )
 }
