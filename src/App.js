87df74b57c8c02dfca084790e7e09bdc43b4225b
Ankita Sawrav <ankita.sawrav@gmail.com>
2023-08-13 21:14:42 +1000

    adding routes and css

--- src/App.js
+++ src/App.js
@@ -1,95 +1,109 @@
-import React, {Component} from 'react'
-import "./index.css"
-import {Route, Routes } from "react-router-dom"
-import Home from "./routes/Home"
-import AdminProfiles from "./routes/AdminProfiles"
-import Footer from "./components/Footer"
-import Navbar from './components/Navbar1'
-import NewForm from "./components/NewForm"
-import AboutUs from './routes/AboutUs'
-import PublicProfiles from "./routes/PublicProfiles"
-import ViewSingleProfile from "./routes/ViewSingleProfile"
-import EditForm from "./routes/EditForm"
-import { LoginPage } from './routes/Login'
-import ContactFormPg from './routes/ContactFormPg'
-import Team from './routes/Team'
-
+import React, { Component } from "react";
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import AdminProfiles from "./routes/AdminProfiles";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar1";
+import NewForm from "./components/NewForm";
+import AboutUs from "./routes/AboutUs";
+import PublicProfiles from "./routes/PublicProfiles";
+import ViewSingleProfile from "./routes/ViewSingleProfile";
+import EditForm from "./routes/EditForm";
+import { LoginPage } from "./routes/Login";
+import ContactFormPg from "./routes/ContactFormPg";
+import Team from "./routes/Team";
+import WhatWeDoComponent from "./routes/WhatWeDoComponent";
 
 // let baseURL = ""
 // if (process.env.NODE_ENV === 'development') {
 //   baseURL = 'http://localhost:3003'
-// } 
+// }
 // else{
 //   baseURL = process.env.REACT_APP_BACKEND_URL
 // }
 
-let  baseURL = process.env.REACT_APP_BACKEND_URL
+let baseURL = process.env.REACT_APP_BACKEND_URL;
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			profiles: [],
+		};
+	}
+	//lifecycle - only run once when the component is mounted for the first time
+	componentDidMount() {
+		this.getProfiles();
+	}
+	getProfiles = () => {
+		fetch(baseURL + "/profiles")
+			//if fetch works
+			.then((res) => {
+				//if response is successful return json
+				if (res.status === 200) {
+					return res.json();
+					//else it was not successful
+				} else {
+					return [];
+				}
+			})
+			.then((data) => {
+				// console.log("data", data);
+				//setState to be that data
+				this.setState({
+					profiles: data.profiles,
+				});
+			});
+	};
 
-  constructor(props) {
-    super(props)
-      this.state = {
-        profiles: []
-      }
-    }
-      //lifecycle - only run once when the component is mounted for the first time
-componentDidMount(){
-  this.getProfiles()
- }
-    getProfiles = () =>{
-      fetch(baseURL + '/profiles')
-        //if fetch works
-        .then((res) => {
-          //if response is successful return json
-          if (res.status === 200) {
-            return res.json();
-            //else it was not successful
-          } else {
-            return [];
-          }
-        }) 
-        .then((data) => {
-          // console.log("data", data);
-          //setState to be that data
-          this.setState({ profiles: data.profiles });
-        });
-      }
+	handleAddProfile = (profiles) => {
+		console.log("profiles inside handleAddProfile", profiles);
+		//copy the entire profiles array to a new array
+		const copyProfiles = [...this.state.profiles];
+		copyProfiles.unshift(profiles);
+		this.setState({
+			profiles: copyProfiles,
+		});
+	};
 
-      handleAddProfile = (profiles) => {
-        console.log("profiles inside handleAddProfile", profiles)
-         //copy the entire profiles array to a new array
-         const copyProfiles = [...this.state.profiles];
-         copyProfiles.unshift(profiles);
-         this.setState({profiles: copyProfiles});
-       };
-
-      
-      render () {
-
-
-
-  return (
-    <div className="App">
-        <>
-      <Navbar />
-      <Routes>
-       <Route exact path="/" index element={<Home/>}/>
-       <Route path="/about" element={<AboutUs/>}/> 
-       <Route path="/contact" element={<ContactFormPg/>}/> 
-       <Route path="/team" element={<Team/>}/> 
-
-       <Route path="/admin-profiles" element={<AdminProfiles profiles={this.state.profiles}/>}/>
-       <Route path="/create" element={<NewForm handleAddProfile={this.handleAddProfile}/>}/>
-       <Route path="/public-profiles" element={<PublicProfiles profiles={this.state.profiles}/>}/>
-       <Route path="/profiles/:id" element={<ViewSingleProfile  profiles={this.state.profiles}   />}/>
-       <Route path="/profiles/:id/edit" element={<EditForm  profiles={this.state.profiles}   />}/>
-       <Route path="/login" element={<LoginPage />} />
-      </Routes>
-      <Footer/>
-     </>
-    </div>
-  );
+	render() {
+		return (
+			<div className="App">
+				<>
+					<Navbar />
+					<Routes>
+						<Route exact path="/" index element={<Home />} />{" "}
+						<Route path="/about" element={<AboutUs />} />{" "}
+						<Route path="/contact" element={<ContactFormPg />} />{" "}
+						<Route path="/team" element={<Team />} />{" "}
+						<Route path="/whatWeDo" element={<WhatWeDoComponent />} />
+						<Route
+							path="/admin-profiles"
+							element={<AdminProfiles profiles={this.state.profiles} />}
+						/>
+						<Route
+							path="/create"
+							element={<NewForm handleAddProfile={this.handleAddProfile} />}
+						/>
+						<Route
+							path="/public-profiles"
+							element={<PublicProfiles profiles={this.state.profiles} />}
+						/>
+						<Route
+							path="/profiles/:id"
+							element={<ViewSingleProfile profiles={this.state.profiles} />}
+						/>
+						<Route
+							path="/profiles/:id/edit"
+							element={<EditForm profiles={this.state.profiles} />}
+						/>
+						<Route path="/login" element={<LoginPage />} />{" "}
+					</Routes>{" "}
+					<Footer />
+				</>{" "}
+			</div>
+		);
+	}
 }
-}
-export default App
+export default App;
---
+++ src/routes/WhatWeDoComponent.js
@@ -0,0 +1,91 @@
+import BecomeDonorBtn from "../components/BecomeDonorBtn";
+import "./WhatWeDoComponent.css";
+import avatar from "../assets/avatar.png";
+
+const WhatWeDoComponent = () => (
+	<div className="what-we-do-comp-container">
+		<div className="header-container">
+			<h1>What GoSaveALife Does</h1>
+			<h2>
+				Raise awareness about the critical need for organ donation and to
+				facilitate the donor matching process.
+			</h2>
+		</div>
+		<div className="features-container">
+			<div className="feature-wrapper">
+				<img src={avatar}></img>
+				<div className="content-wrapper">
+					<h1>Recruit Donors</h1>
+					<h2>
+						My life took an incredible turn when she discovered GoSaveALife. In
+						a time of desperation, this non-profit organization brought assisted
+						Lisa to become a kidney donor for her brother. Guided by their
+						support and resources, she embarked on a life-altering journey,
+						ultimately saving her brother's life. -Lisa Smith
+					</h2>
+					<BecomeDonorBtn></BecomeDonorBtn>
+				</div>
+			</div>
+		</div>
+		<div className="features-container">
+			<div className="feature-wrapper">
+				<img src={avatar}></img>
+				<div className="content-wrapper">
+					<h1>Recruit Donors</h1>
+					<h2>
+						My life took an incredible turn when she discovered GoSaveALife. In
+						a time of desperation, this non-profit organization brought assisted
+						Lisa to become a kidney donor for her brother. Guided by their
+						support and resources, she embarked on a life-altering journey,
+						ultimately saving her brother's life. -Lisa Smith
+					</h2>
+					<BecomeDonorBtn></BecomeDonorBtn>
+				</div>
+			</div>
+		</div>
+		<div className="features-container">
+			<div className="feature-wrapper">
+				<img src={avatar}></img>
+				<div className="content-wrapper">
+					<h1>Find Candidates</h1>
+					<h2>Testimony #2</h2>
+					<BecomeDonorBtn></BecomeDonorBtn>
+				</div>
+			</div>
+		</div>
+		<div className="features-container">
+			<div className="feature-wrapper">
+				<img src={avatar}></img>
+				<div className="content-wrapper">
+					<h1>Spread Awareness</h1>
+					<h2>
+						I want to express my deepest gratitude to GoSaveALife for their
+						incredible efforts in educating and raising awareness about kidney
+						donations in our community. As someone who witnessed firsthand the
+						impact of their community events and platforms, I can confidently
+						say that they have made a significant difference in the lives of
+						those in need.-Chris Thomas
+					</h2>
+					<BecomeDonorBtn></BecomeDonorBtn>
+				</div>
+			</div>
+		</div>
+		<div className="features-container">
+			<div className="feature-wrapper">
+				<img src={avatar}></img>
+				<div className="content-wrapper">
+					<h1>Raise Money</h1>
+					<h2>
+						GoSaveALife has successfully raised an impressive (x amount) of
+						funding, facilitating meaningful connections between kidney donors
+						and recipients, and bringing hope to those in need of life-saving
+						transplants.
+					</h2>
+					<BecomeDonorBtn></BecomeDonorBtn>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default WhatWeDoComponent;
