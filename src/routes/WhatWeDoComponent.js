8de72f4ada349809ce160745def1b048be4220c9
Ankita Sawrav <ankita.sawrav@gmail.com>
2023-08-14 00:06:24 +1000

    added css

--- src/routes/WhatWeDoComponent.js
+++ src/routes/WhatWeDoComponent.js
@@ -1,4 +1,4 @@
-import BecomeDonorBtn from "../components/BecomeDonorBtn";
+import ButtonComponent from "../components/ButtonComponent";
 import "./WhatWeDoComponent.css";
 import avatar from "../assets/avatar.png";
 
@@ -21,39 +21,21 @@ const WhatWeDoComponent = () => (
 						a time of desperation, this non-profit organization brought assisted
 						Lisa to become a kidney donor for her brother. Guided by their
 						support and resources, she embarked on a life-altering journey,
-						ultimately saving her brother's life. -Lisa Smith
+						ultimately saving her brother's life.
+						<br />
+						<br /> -Lisa Smith
 					</h2>
-					<BecomeDonorBtn></BecomeDonorBtn>
+					<ButtonComponent text="Become A Donar"></ButtonComponent>
 				</div>
 			</div>
-		</div>
-		<div className="features-container">
-			<div className="feature-wrapper">
-				<img src={avatar}></img>
-				<div className="content-wrapper">
-					<h1>Recruit Donors</h1>
-					<h2>
-						My life took an incredible turn when she discovered GoSaveALife. In
-						a time of desperation, this non-profit organization brought assisted
-						Lisa to become a kidney donor for her brother. Guided by their
-						support and resources, she embarked on a life-altering journey,
-						ultimately saving her brother's life. -Lisa Smith
-					</h2>
-					<BecomeDonorBtn></BecomeDonorBtn>
-				</div>
-			</div>
-		</div>
-		<div className="features-container">
 			<div className="feature-wrapper">
 				<img src={avatar}></img>
 				<div className="content-wrapper">
 					<h1>Find Candidates</h1>
 					<h2>Testimony #2</h2>
-					<BecomeDonorBtn></BecomeDonorBtn>
+					<ButtonComponent text="View Candidates"></ButtonComponent>
 				</div>
 			</div>
-		</div>
-		<div className="features-container">
 			<div className="feature-wrapper">
 				<img src={avatar}></img>
 				<div className="content-wrapper">
@@ -64,13 +46,12 @@ const WhatWeDoComponent = () => (
 						donations in our community. As someone who witnessed firsthand the
 						impact of their community events and platforms, I can confidently
 						say that they have made a significant difference in the lives of
-						those in need.-Chris Thomas
+						those in need. <br />
+						<br />
+						-Chris Thomas
 					</h2>
-					<BecomeDonorBtn></BecomeDonorBtn>
 				</div>
 			</div>
-		</div>
-		<div className="features-container">
 			<div className="feature-wrapper">
 				<img src={avatar}></img>
 				<div className="content-wrapper">
@@ -81,7 +62,7 @@ const WhatWeDoComponent = () => (
 						and recipients, and bringing hope to those in need of life-saving
 						transplants.
 					</h2>
-					<BecomeDonorBtn></BecomeDonorBtn>
+					<ButtonComponent text="Contribute"></ButtonComponent>
 				</div>
 			</div>
 		</div>
